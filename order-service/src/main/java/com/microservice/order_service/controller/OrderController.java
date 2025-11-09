c309693651de52743046fe27276c82b64dbfacd0
Malkith99 <malkithamanda@gmail.com>
2025-11-09 18:05:41 +0530

    update the ordercontroller

---
+++ order-service/src/main/java/com/microservice/order_service/controller/OrderController.java
@@ -0,0 +1,28 @@
+package com.microservice.order_service.controller;
+
+import com.microservice.order_service.dto.OrderRequest;
+import com.microservice.order_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
+        orderService.placeOrder(orderRequest);
+        return "Order Placed Successfully";
+    }
+
+
+}
